08d0abe3953fd83fbd41976dc10f9a2e6b97639b
Nathan Regaudie <nathanregaudie@gmail.com>
2022-10-31 15:43:44 +0100

    Ajout du type pile contenant des élements de type case_t

---
+++ type/pile.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include "case.h"
+#include "pile.h"
+
+
+pile * pile_vide() {
+    return NULL;
+}
+
+pile * empiler(case_t c, pile *p) {
+    pile * res = (pile *)malloc(sizeof(pile));
+    res->sommet = c;
+    res->suivant = p;
+    return res;
+}
+
+bool pile_est_vide(pile *p) {
+    return p == NULL;
+}
+
+case_t sommet(pile *p) {
+    if (pile_est_vide(p)) {
+        printf("Erreur: sommet(pile p) : p est vide\n");
+        exit(EXIT_FAILURE);
+    }
+    return p->sommet;
+}
+
+case_t depiler(pile **p) {
+    pile *tmp;
+    case_t s;
+    if (pile_est_vide(*p)) {
+        printf("Erreur: depiler(pile p) : p est vide\n");
+        exit(EXIT_FAILURE);
+    }
+    s = sommet(*p);
+    tmp = *p;
+    *p = (*p)->suivant;
+    free(tmp);
+    return s;
+}
+
+void pile_liberer(pile *p) {
+    if (!pile_est_vide(p)) {
+        pile_liberer(p->suivant);
+        free(p);
+    }
+}
---
+++ type/pile.h
@@ -0,0 +1,24 @@
+#ifndef PILE_H
+#define PILE_H
+
+#include <stdbool.h>
+#include "case.h"
+#include "pile.h"
+
+
+typedef struct pile_s pile;
+struct pile_s {
+    case_t sommet;
+    pile *suivant;
+};
+
+
+bool pile_est_vide(pile *p);
+pile * pile_vide();
+pile * empiler(case_t c, pile *p);
+case_t sommet(pile *p);
+case_t depiler(pile **p);
+void pile_liberer(pile *p);
+
+
+#endif
