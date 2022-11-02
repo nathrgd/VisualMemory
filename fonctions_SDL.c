4e14c492cf0550f26bf46fd3d72686320a699bbf
REGAUDIE Nathan <nathan.regaudie9@etu.univ-lorraine.fr>
2022-11-02 16:58:30 +0100

    fonctions_SDL.c : libération des surfaces utilisées pour charger les images

--- fonctions_SDL.c
+++ fonctions_SDL.c
@@ -46,6 +46,7 @@ SDL_Texture * charger_image (const char* nomfichier, SDL_Renderer* renderer)
 {
     SDL_Surface * surface = SDL_LoadBMP(nomfichier);
     SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
     return texture;
 }
 
@@ -56,6 +57,7 @@ SDL_Texture * charger_image_transparente(const char* nomfichier,
     SDL_Surface * surface = SDL_LoadBMP(nomfichier);
     SDL_SetColorKey(surface, SDL_TRUE, SDL_MapRGB(surface->format, r, g, b));
     SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
 
     return texture;
 }
@@ -65,5 +67,6 @@ SDL_Texture * charger_texte(const char* message, SDL_Renderer* renderer,
 {
     SDL_Surface *surface = TTF_RenderText_Solid(font, message, color);
     SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
     return texture;
 }
